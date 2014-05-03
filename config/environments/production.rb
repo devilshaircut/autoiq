b049b24566f85ee0ef1ad8612b92fa9927f47b0c
Mitchell Lane <mitchell@fullscreen.net>
2014-05-02 23:00:30 -0400

    Fixing prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,8 +23,8 @@ Shell::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.compress = true
+  config.assets.js_compressor = Uglifier.new(:mangle => false) if defined? Uglifier
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
