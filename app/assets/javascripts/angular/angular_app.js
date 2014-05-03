856900b80a6c1d0811420b1ef60c1b0015cec1e8
Jason B <devilshaircut@gmail.com>
2014-05-02 21:45:46 -0400

    First commit for AutoIQ.:

---
+++ app/assets/javascripts/angular/angular_app.js
@@ -0,0 +1,13 @@
+// Create main Angular module/app.
+var AngularApp = angular.module("AngularApp", ["ngRoute", "ngAnimate"]);
+
+// Global Angular JavaScript code initialization.
+// AngularApp.run(["$rootScope", function($rootScope) {  }]);
+
+// Placeholder for a global filter.
+// AngularApp.filter('filterName', function () {  });
+
+
+
+
+
