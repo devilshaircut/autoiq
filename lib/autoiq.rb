e80d4b9f3ce24a00057f980a2329c37f48c00545
Francis Plaza <francis@francis.ph>
2014-05-03 11:38:44 -0400

    get inventory by zipcode

--- lib/autoiq.rb
+++ lib/autoiq.rb
@@ -1,8 +1,31 @@
 module Autoiq  
   class << self
     
-    def access_token
+    def access_token(renew=false)
+      Rails.cache.delete([self, "access_token"]) if renew
       Rails.cache.fetch([self, "access_token"]) { get_access_token_from_edmunds }
+    end
+    
+    def inventory_by_zipcode(opts={})
+      raise ArgumentError, 'zipcode missing' if opts[:zipcode].blank?
+      raise ArgumentError, 'radius missing' if opts[:radius].blank?
+      raise ArgumentError, 'make missing' if opts[:make].blank?
+      
+      body = ""
+      opts.each{ |k,v| body += "#{k}=#{v}&"}      
+      request = Typhoeus::Request.new(
+        EDMUNDS_ENDPOINT + '/api/inventory/v1/getall?' + body[0..-2],
+        method: :get,
+        headers: { "Authorization" => "Bearer " + self.access_token }
+      )
+      response = request.run
+      
+      if response.success?
+        return JSON.parse(response.body, :symbolize_names => true)
+      else
+        self.access_token(renew=true)
+        return inventory_by_zipcode(opts)
+      end
     end
     
     private
