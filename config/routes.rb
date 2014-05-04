e24a6afadb4c75793107ee319a5ce81b1a5bc11b
Mitchell Lane <mitchell@fullscreen.net>
2014-05-04 01:13:14 -0400

    Asset stuff

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ end
 Shell::Application.routes.draw do
 
   root :to => "home#index"
-  get   '*path'     => "home#index", :constraints => RouteConstraint.new
+  get '*path' => "home#index", :constraints => RouteConstraint.new
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
