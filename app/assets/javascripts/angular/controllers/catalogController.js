35cecd9f63a2aaf17c0847c3339c5621ab145fa1
Jason B <devilshaircut@gmail.com>
2014-05-03 23:48:23 -0400

    Add more progress to the catalog.

--- app/assets/javascripts/angular/controllers/catalogController.js
+++ app/assets/javascripts/angular/controllers/catalogController.js
@@ -1,19 +1,21 @@
-AngularApp.controller("sampleController", ["$scope", "httpService", function($scope, httpService) {
+AngularApp.controller("catalogController", ["$scope", "httpService", function($scope, httpService) {
 
-  $scope.model = {};
+  // $scope.model = {};
 
-  var apiEndpoint = '/path/to/api/file.json';
+  // var apiEndpoint = '/path/to/api/file.json';
 
-  // This is the callback function that executes if the HTTP requests returns successfully.
-  var getModelSuccess = function(payload, status) {
-    $scope.model = payload.data; // Remove .data if API is not formatted to store data in a data property.
-  };
+  // // This is the callback function that executes if the HTTP requests returns successfully.
+  // var getModelSuccess = function(payload, status) {
+  //   $scope.model = payload.data; // Remove .data if API is not formatted to store data in a data property.
+  // };
 
-  // This is the callback function that executes if the HTTP requests returns unsuccessfully.
-  var getModelFailure = function(payload, status) {};
+  // // This is the callback function that executes if the HTTP requests returns unsuccessfully.
+  // var getModelFailure = function(payload, status) {};
 
-  // Initiate the HTTP request.
-  httpService.getApiEndpoint(apiEndpoint).success(getModelSuccess);
+  // // Initiate the HTTP request.
+  // httpService.getApiEndpoint(apiEndpoint).success(getModelSuccess);
+
+  $scope.tableView = false;
 
 }]);
 
